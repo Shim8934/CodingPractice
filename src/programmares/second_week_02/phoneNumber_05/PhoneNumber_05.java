6d81da663f78642644655782c703960d99d73ad4
ShimKiYoung <shim8934@gmail.com>
2022-04-30 01:34:47 +0900

    2주 차 5번 전화번호 목록 1중 for문 풀이 Commit

--- src/programmares/second_week_02/phoneNumber_05/PhoneNumber_05.java
+++ src/programmares/second_week_02/phoneNumber_05/PhoneNumber_05.java
@@ -38,13 +38,11 @@ public class PhoneNumber_05 {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
 
-        HashSet<String> phNum = new HashSet<>(Arrays.asList(phone_book));
+        Arrays.sort(phone_book);
 
-        for (String ph_number : phone_book) {
-            for (String num : phNum) {
-                if (ph_number.equals(num)== false && num.startsWith(ph_number)) {
-                    return false;
-                }
+        for (int i = 0; i< phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
             }
         }
 
